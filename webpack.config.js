df0f5d86a15abde4632a36df1db93dc650cb368d
Pedro Piñera <pepibumur@gmail.com>
2018-01-28 14:25:38 -0500

    [webpack-react] Add Typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 module.exports = {
-  entry: "./webpack/index.js",
+  entry: "./webpack/index.tsx",
   output: {
     path: __dirname + "/assets/js",
     filename: "bundle.js"
   },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
   module: {
-  loaders: [
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules)/,
-      loader: "babel-loader",
-      query: {
-        presets: ["react", "es2015"]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: {
+          presets: ["react", "es2015"]
+        }
       }
-    }
     ]
   }
 };
