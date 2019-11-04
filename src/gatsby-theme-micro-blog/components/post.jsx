77fba57955b9b152d936707afde129780a878eb1
Pedro Piñera <pedro@ppinera.es>
2019-11-04 07:07:18 +0100

    Nicer the dates UI

--- src/gatsby-theme-micro-blog/components/post.jsx
+++ src/gatsby-theme-micro-blog/components/post.jsx
@@ -1,8 +1,12 @@
 /** @jsx jsx */
 import { jsx, Styled } from "theme-ui"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import moment from "moment"
 
 export default ({ date, body, tags }) => {
+  const dateMoment = moment(date, "MMMM Do YYYY")
+  const dateMonth = dateMoment.format("MMMM YYYY")
+  const dateDay = dateMoment.format("DD")
   return (
     <article
       sx={{
@@ -13,7 +17,20 @@ export default ({ date, body, tags }) => {
       }}
     >
       <header sx={{ mb: 4 }}>
-        <Styled.h3 sx={{ mb: 3 }}>{date}</Styled.h3>
+        <Styled.h3 sx={{ mb: 3 }}>
+          <span
+            sx={{
+              bg: "primary",
+              color: "background",
+              pl: "4px",
+              pr: "6px",
+              borderRadius: "26px",
+            }}
+          >
+            {dateDay}
+          </span>{" "}
+          {dateMonth}
+        </Styled.h3>
         <div
           sx={{
             marginRight: 2,
