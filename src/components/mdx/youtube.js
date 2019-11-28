8170d921114f693394db5051ff6241e48fb62d17
Pedro Piñera <pedro@ppinera.es>
2019-11-28 14:35:24 +0100

    Fix the issue with the youtube video

--- src/components/mdx/youtube.js
+++ src/components/mdx/youtube.js
@@ -1,14 +1,18 @@
+/** @jsx jsx */
+import { jsx, Styled } from "theme-ui"
 import React from "react"
 
 export default ({ id }) => {
   return (
     <iframe
-      width="560"
-      height="315"
+      sx={{
+        width: ["100%", 560],
+        height: 315,
+      }}
       src={`https://www.youtube-nocookie.com/embed/${id}`}
-      frameborder="0"
+      frameBorder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
+      allowFullScreen
     ></iframe>
   )
 }
